79bb2699509b941d718cbfe54a0a3f352d2d8eae
ShuaiHua Du <shuaihua.du@outlook.com>
2023-03-12 01:37:25 +0800

    Adjust the chat logic for process flow.

--- teams-bot-openai/bot/teamsBot.js
+++ teams-bot-openai/bot/teamsBot.js
@@ -1,59 +1,77 @@
-const { TeamsActivityHandler, CardFactory, TurnContext } = require("botbuilder");
+const { TeamsActivityHandler, CardFactory, TurnContext, MessageFactory } = require("botbuilder");
+const { ActionTypes } = require('botframework-schema');
 const rawWelcomeCard = require("./adaptiveCards/welcome.json");
-const rawLearnCard = require("./adaptiveCards/learn.json");
+const rawAudioCard = require("./adaptiveCards/audio.json");
 const cardTools = require("@microsoft/adaptivecards-tools");
 const config = require("./config");
 const { Configuration, OpenAIApi } = require("openai");
 const speechSdk = require("microsoft-cognitiveservices-speech-sdk");
 const { BlobServiceClient } = require("@azure/storage-blob");
 const uuidv1 = require('uuidv1');
+const lodash = require("lodash")
 
 
 const voiceNames = [
-  { name: "普通话", value: "zh-CN-YunjianNeural" },
-  { name: "粤语", value: "yue-CN-XiaoMinNeural" },
-  { name: "河南话", value: "zh-CN-henan-YundengNeural" },
-  { name: "东北话", value: "zh-CN-liaoning-XiaobeiNeural" }
+  { type: ActionTypes.ImBack, title: "普通话", value: "zh-CN-YunjianNeural" },
+  { type: ActionTypes.ImBack, title: "粤语", value: "yue-CN-XiaoMinNeural" },
+  { type: ActionTypes.ImBack, title: "河南话", value: "zh-CN-henan-YundengNeural" }
+  //{ type: ActionTypes.ImBack, title: "东北话", value: "zh-CN-liaoning-XiaobeiNeural" },
+  //Suggested Actions only show there on Teams
 ];
+
+const voiceNameTitles = lodash.map(voiceNames, "title");
 
 class TeamsBot extends TeamsActivityHandler {
   constructor() {
     super();
-
-    this.audioFileObj = { url: "" };
-
+    this.chatData = { url: "", voiceName: voiceNames[0].title, lastGPTAnswer: "" };
     const configuration = new Configuration({
       apiKey: config.openaiApiKey
     });
 
     const openai = new OpenAIApi(configuration);
-
     this.onMessage(async (context, next) => {
       console.log("Running with Message Activity.");
+
       let txt = context.activity.text;
-      //console.log(context);
       const removedMentionText = TurnContext.removeRecipientMention(context.activity);
       if (removedMentionText) {
         // Remove the line break
         txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
       }
-      // Call the open ai to get response
-      // const response = await openai.createCompletion({
-      //   model: "text-davinci-003",
-      //   prompt: txt,
-      //   temperature: 0,
-      //   max_tokens: 2048
-      // });
-      // console.log(response);
-      // await context.sendActivity(response.data.choices[0].text);
       if (txt === "hello") {
         const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
         await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
       } else {
-        this.textToSpeech(txt, (audioFileUrl) => {
-          this.audioFileObj.url = audioFileUrl;
-          console.log(this.audioFileObj);
-        });
+        if (voiceNameTitles.includes(txt)) {
+          var voiceName = lodash.find(voiceNames, function (v) {
+            return v.title === txt;
+          });
+          if (!voiceName) {
+            voiceName = voiceNames[0];
+          }
+          await this.textToSpeech(this.chatData, (url) => {
+            console.log(this.chatData);
+            console.log(context);
+            console.log(url);
+          });
+          // const card = cardTools.AdaptiveCards.declare(rawAudioCard).render(this.chatData);
+          // await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
+        }
+        else {
+          // Send request by the open ai sdk and get response
+          // const response = await openai.createCompletion({
+          //   model: "text-davinci-003",
+          //   prompt: txt,
+          //   temperature: 0,
+          //   max_tokens: 2048
+          // });
+          // console.log(response);
+          // await context.sendActivity(response.data.choices[0].text);
+          this.chatData.lastGPTAnswer = "response.data.choices[0].text" + uuidv1();
+          await context.sendActivity(this.chatData.lastGPTAnswer);
+          await this.sendSuggestedActions(context);
+        }
       }
       // By calling next() you ensure that the next BotHandler is run.
       await next();
@@ -63,53 +81,57 @@ class TeamsBot extends TeamsActivityHandler {
       const membersAdded = context.activity.membersAdded;
       for (let cnt = 0; cnt < membersAdded.length; cnt++) {
         if (membersAdded[cnt].id) {
-          const card1 = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
-          const card2 = cardTools.AdaptiveCards.declareWithoutData(rawLearnCard).render();
-          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card1), CardFactory.adaptiveCard(card2)] });
+          const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
+          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
           break;
         }
       }
       await next();
     });
   }
-
-
-  textToSpeech(txt, callback) {
+  async textToSpeech(txt, voiceName, callback) {
     // 1.Process the open ai data result to speech
     // 2.Upload the file to Azure Blob Storage
     // 3.Send the file url to a card in teams
     const audioFile = "voice" + uuidv1() + ".wav";
     const speechConfig = speechSdk.SpeechConfig.fromSubscription(config.cognitiveServiceKey, config.cognitiveServiceRegion);
     const audioConfig = speechSdk.AudioConfig.fromAudioFileOutput(audioFile);
-    speechConfig.speechSynthesisVoiceName = voiceNames[0].value;
+    if (!voiceName) {
+      voiceName = voiceNames[0].value;
+    }
+    speechConfig.speechSynthesisVoiceName = voiceName;
 
     var synthesizer = new speechSdk.SpeechSynthesizer(speechConfig, audioConfig);
-    synthesizer.speakTextAsync(txt,
-      function (result) {
-        if (result.reason === speechSdk.ResultReason.SynthesizingAudioCompleted) {
-          //Upload the audio file to azure blob storage which convert by Azure Text To Speech
-          const blobServiceClient = BlobServiceClient.fromConnectionString(config.azureStorageConnectionString);
-          const containerClient = blobServiceClient.getContainerClient(config.azureStorageAccountContainerName);
-          const blockBlobClient = containerClient.getBlockBlobClient(audioFile);
-          var result = blockBlobClient.uploadFile(audioFile);
-          callback(blockBlobClient.url);
-          console.log("synthesis finished.");
-          //delete the audio file after 60s
-          // setTimeout(() => {
-          //   fs.rm(audioFile);
-          // }, 60000);
-        } else {
-          console.error("Speech synthesis canceled, " + result.errorDetails +
-            "\nDid you set the speech resource key and region values?");
-        }
-        synthesizer.close();
-        synthesizer = null;
-      },
+    synthesizer.speakTextAsync(txt, async (result) => {
+      if (result.reason === speechSdk.ResultReason.SynthesizingAudioCompleted) {
+        //Upload the audio file to azure blob storage which convert by Azure Text To Speech
+        const blobServiceClient = BlobServiceClient.fromConnectionString(config.azureStorageConnectionString);
+        const containerClient = blobServiceClient.getContainerClient(config.azureStorageAccountContainerName);
+        const blockBlobClient = containerClient.getBlockBlobClient(audioFile);
+        var result = blockBlobClient.uploadFile(audioFile);
+        this.chatData.url = blockBlobClient.url;
+        callback(blockBlobClient.url)
+        //delete the audio file after 60s
+        // setTimeout(() => {
+        //   fs.rm(audioFile);
+        // }, 60000);
+      } else {
+        console.error("Speech synthesis canceled, " + result.errorDetails +
+          "\nDid you set the speech resource key and region values?");
+      }
+      synthesizer.close();
+      synthesizer = null;
+    },
       function (err) {
         console.trace("err - " + err);
         synthesizer.close();
         synthesizer = null;
       });
+  }
+  async sendSuggestedActions(context) {
+    var reply = MessageFactory.text("我还可以将我的回答转换成语音，您如果需要的话可以点击下面的语音选项，我会按照您的选择进行转换。");
+    reply.suggestedActions = { "actions": voiceNames, "to": [context.activity.from.id] };
+    await context.sendActivity(reply);
   }
 }
 
